39bde327f41191e3fc050a5398e3a6f3b0d9c1ef
Rémi Labeyrie <calops@tocards.net>
2024-10-28 22:30:50 +0100

    factorize cachix action

--- flake.nix
+++ flake.nix
@@ -12,18 +12,21 @@
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
       ];
+
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "aarch64-darwin"
         "x86_64-darwin"
       ];
+
       perSystem =
         { config, ... }:
         {
           imports = [ ./packages ];
           overlayAttrs = config.packages;
         };
+
       flake = {
         # formatter = inputs.pkgs.nixfmt-rfc-style; FIXME
       };
