97b253d1c474764d791fd290a3240682bab4d9b8
Rémi Labeyrie <calops@tocards.net>
2024-10-29 10:10:14 +0100

    use rev in version

--- packages/kitty.nix
+++ packages/kitty.nix
@@ -1,18 +1,21 @@
 { pkgs, ... }:
+let
+  rev = "8b7cd98a0eb482a5fa26daef90be7db5fd1be64d";
+in
 pkgs.kitty.overrideAttrs (oldAttrs: rec {
   name = "kitty-nightly";
-  version = "nightly2";
+  version = "nightly-${rev}";
 
   src = pkgs.fetchFromGitHub {
+    inherit rev;
     owner = "kovidgoyal";
     repo = "kitty";
-    rev = "8b7cd98a0eb482a5fa26daef90be7db5fd1be64d";
     sha256 = "sha256-ze28vZJFjTTCZHoGoNvW9GZVY45EnOTPlTwR11Z5NYo=";
   };
 
   goModules =
     (pkgs.buildGo123Module {
-      pname = "kitty-go-modules";
+      pname = "kitty-go-modules-nightly";
       inherit src version;
       vendorHash = "sha256-d5jRhOm53HDGnsU5Lg5tVGU/9z8RGqORzS53hOyIKBk=";
       doCheck = false;
