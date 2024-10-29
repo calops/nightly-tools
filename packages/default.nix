5090103fe25820049e127934fee30b70577bfb30
Rémi Labeyrie <calops@tocards.net>
2024-10-29 10:01:46 +0100

    use callpackage instead of modules

--- packages/default.nix
+++ packages/default.nix
@@ -5,6 +5,8 @@
   ...
 }:
 {
+  packages.kitty-nightly = pkgs.callPackage ./kitty.nix { };
+
   packages.all = pkgs.symlinkJoin {
     name = "all";
     paths = builtins.attrValues (lib.filterAttrs (name: _: name != "all") config.packages);
