d12e62c4b4179b3cc93aa980f6ded67d68441e46
Rémi Labeyrie <calops@tocards.net>
2024-10-31 12:05:19 +0100

    remove niv and use flake only

---
+++ update-sources.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env bash
+
+set -e
+set -x
+
+function pre-update() {
+	kitty-pre-update
+}
+
+function update() {
+	nix flake update
+}
+
+function post-update() {
+	kitty-post-update
+}
+
+# Kitty vendorHash requires some manual intervention
+function get_kitty_vendor_hash() {
+	nix run nixpkgs#nix-prefetch -- --option extra-experimental-features flakes \
+		'{ sha256 }: (builtins.getFlake (toString ./.)).packages."${pkgs.system}".kitty.goModules.overrideAttrs (_: { vendorHash = sha256; })'
+}
+
+function kitty-pre-update() {
+	OLD_VENDOR_HASH=$(sed -n 's|.*vendorHash = "\(.*\)";.*|\1|p' packages/kitty.nix)
+}
+
+function kitty-post-update() {
+	NEW_VENDOR_HASH=$(get_kitty_vendor_hash)
+	sed -i "s|${OLD_VENDOR_HASH}|${NEW_VENDOR_HASH}|" packages/kitty.nix
+}
+
+pre-update
+update
+post-update
