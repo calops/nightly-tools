820bf454c3dd756064b98618be018d08621b183d
Rémi Labeyrie <calops@tocards.net>
2024-10-31 08:49:29 +0100

    not working

--- nix/update-sources.sh
+++ nix/update-sources.sh
@@ -3,4 +3,10 @@
 set -e
 set -x
 
-nix run nixpkgs#niv update
+nix run nixpkgs#niv -- update
+
+# Kitty vendorHash requires some manual intervention
+VENDOR_HASH=$(nix run nixpkgs#nix-prefetch -- "{ sha256 }: pkgs.kitty.goModules.overrideAttrs (_: { vendorHash = sha256; })" --option extra-experimental-features flakes)
+OLD_HASH=$(jq -r '.kitty.vendorHash' nix/sources.json)
+
+sed -i "s|${OLD_HASH}|${VENDOR_HASH}|" nix/sources.json
