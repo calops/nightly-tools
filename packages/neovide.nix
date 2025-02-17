7432bffc2ba79eafbf580e6b7b52bfe233547be7
Rémi Labeyrie <calops@tocards.net>
2025-02-17 17:14:59 +0100

    remove checks for neovide

--- packages/neovide.nix
+++ packages/neovide.nix
@@ -2,10 +2,10 @@
 {
   pkgs,
   neovim,
-  ...
 }:
-pkgs.neovide.overrideAttrs (oldAttrs: {
+pkgs.neovide.overrideAttrs {
   inherit neovim;
   name = "neovide-nightly";
   version = "nightly";
-})
+  doCheck = false;
+}
