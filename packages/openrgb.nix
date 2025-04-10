af942f69652fbc34fe1d267f7dda5dc6a0b48632
Rémi Labeyrie <calops@tocards.net>
2025-04-10 22:52:14 +0200

    fix version name for openrgb

--- packages/openrgb.nix
+++ packages/openrgb.nix
@@ -1,4 +1,6 @@
 { openrgb-with-all-plugins, src }:
-openrgb-with-all-plugins.overrideAttrs {
+openrgb-with-all-plugins.overrideAttrs (super: {
+  version = "nightly";
+  name = "${super.name}-nightly";
   inherit src;
-}
+})
